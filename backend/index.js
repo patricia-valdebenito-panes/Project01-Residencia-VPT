5248928cd616e45ccc720c16cec976f609d6ec56
patricia-valdebenito-panes <153689602+patricia-valdebenito-panes@users.noreply.github.com>
2023-12-13 17:09:27 +0000

    add - setting connect db.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 //
-const PORT = 4000; // env
-const app = express();
-import express from 'express';
 
+import express from 'express';
+import dotenv from 'dotenv';
+import ConectDB from './config/db.js';
+
+const app = express();
+dotenv.config();
+
+ConectDB()
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=>{
     console.log(`servidor corriendo en puerto ${PORT}`)
 })
+
+
 
