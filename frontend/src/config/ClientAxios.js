60492cd1376de7fde06573644bf07fa99063e406
patricia-valdebenito-panes <p.valdebenitopanes@uandresbello.edu>
2023-12-24 17:42:51 -0300

    context for data handling is worked out according to view

--- frontend/src/config/ClientAxios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const ClientAxios = axios.create({
-baseURL :`${import.meta.env.VITE_HOST_API}/api`
-})
-
-
-export default ClientAxios;
--- frontend/src/pages/Templates.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import useAuth from '../hooks/useAuth'
-
-export const Templates = () => {
-  return (<>
-   <div>Templates</div>
-   </>)
-}
